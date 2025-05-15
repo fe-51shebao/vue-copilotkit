262bca6f6c9fddb15a0bf0bdaef8280fea5bd671
zhengwenfeng <zhengwenfeng@51shebao.com>
2025-05-15 17:39:01 +0800

    修改npm打包方式，解决npm包引入问题

--- packages/vue-core/vite.config.ts
+++ packages/vue-core/vite.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from '@copilotkit/vite-config'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: ['vue','@copilotkit/runtime-client-gql','@copilotkit/shared']
+        }
+    }
+})
--- packages/vue-textarea/vite.config.ts
+++ packages/vue-textarea/vite.config.ts
@@ -2,4 +2,10 @@ import { defineConfig } from '@copilotkit/vite-config'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: ['vue','@copilotkit/vue-core','@copilotkit/runtime-client-gql','@copilotkit/shared','element-plus']
+        }
+    }
+})
